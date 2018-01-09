5629dbdb31b47f6e1ed5d862a9b9108124b10bda
Sagie <sagielevy21@gmail.com>
2018-01-09 21:37:28 +0200

    Add skeleton of Generator solution. Update rules and comments file

---
+++ CFG Generator/CFGGeneratorCore/Tracery/TraceryWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFGGeneratorCore.Tracery
+{
+    /// <summary>
+    /// Wrap tracery js function calls
+    /// </summary>
+    public class TraceryWrapper
+    {
+        
+    }
+}
