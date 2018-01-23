1bd7fddd47a69f208b025e286525663db11c9f65
Nirki <niryyy@gmail.com>
2018-01-23 18:59:26 +0200

    tester merged projects

--- CFG Generator/Tester/Program.cs
+++ CFG Generator/Tester/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CFGGeneratorCoreDll.Generator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace Tester
         static void Main(string[] args)
         {
             // TODO Test your logic here
+            AsyncCFGGenerator asyncCFGGenerator;
         }
     }
 }
