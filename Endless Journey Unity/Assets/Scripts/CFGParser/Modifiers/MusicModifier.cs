0257f945696bbcaf07fb90f3293780600db59144
TF <tom.feigin@gmail.com>
2018-01-26 22:15:44 +0200

    Changed music indices to be zero based

--- Endless Journey Unity/Assets/Scripts/CFGParser/Modifiers/MusicModifier.cs
+++ Endless Journey Unity/Assets/Scripts/CFGParser/Modifiers/MusicModifier.cs
@@ -10,16 +10,44 @@ namespace Assets.Scripts.CFGParser.Modifiers
     class MusicModifier : IWorldModifier//<IMusicData>
     {
         GameObject musicAudioSources;
+        
+        float startTime;
+        Dictionary<string, float> originalVolumes;
 
         public MusicModifier(GameObject musicAudioSources)
         {
+            originalVolumes = new Dictionary<string, float>();
             this.musicAudioSources = musicAudioSources;
+            this.startTime = Time.time;
+            AudioSource[] sources = musicAudioSources.GetComponentsInChildren<AudioSource>();
+            for (int i = 0; i < sources.Length; i++)
+            {
+                originalVolumes[sources[i].name] = sources[i].volume;
+            }
         }
 
         public void ModifySection(ISentenceData data)
         {
             var musicData = data as IMusicData;
-            throw new NotImplementedException();
+            MusicTrack[] tracks = musicData.MusicTracks();
+            AudioSource[] sources = musicAudioSources.GetComponentsInChildren<AudioSource>();
+            for (int i = 0; i < sources.Length; i++)
+            {
+                // TODO: Using tracks length in such a fasion assumes that the number of 'a' tracks is the same number
+                // of 'b' tracks and the same number of 'percussion' tracks etc. If we want a different size array for each
+                // type of track this code needs to be changed accordingly.
+                int trackIndex = i / tracks.Length;
+                float vol = 0;
+                int innerIndex = i % tracks.Length;
+                if(innerIndex  == tracks[trackIndex].track)
+                {
+                    Debug.Log("I: " + i.ToString() + " track index: " + trackIndex.ToString() + " Inner index: " + innerIndex.ToString());
+                    vol = tracks[trackIndex].vol;
+                }
+                
+                var newVolume = Mathf.Lerp(originalVolumes[sources[i].name], vol, Globals.speedChange * 2 * (Time.time - startTime));
+                sources[i].volume = newVolume;
+            }
         }
     }
 }
